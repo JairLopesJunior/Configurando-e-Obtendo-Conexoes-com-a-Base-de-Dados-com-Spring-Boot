b77b1484509396403d553dcbc1e20da6bbdccf42
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-04-18 13:19:57 -0300

    Salvando e recuperando clientes

--- api-spring-boot-udemy/src/main/java/api/spring/boot/udemy/VendasApplication.java
+++ api-spring-boot-udemy/src/main/java/api/spring/boot/udemy/VendasApplication.java
@@ -1,5 +1,7 @@
 package api.spring.boot.udemy;
 
+import api.spring.boot.udemy.domain.entity.Cliente;
+import api.spring.boot.udemy.domain.repository.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,9 +12,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+          Cliente cliente = new Cliente();
+          cliente.setNome("Jair");
+          clientes.salvar(cliente);
+
+            Cliente cliente2 = new Cliente();
+            cliente2.setNome("Joao");
+            clientes.salvar(cliente2);
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
